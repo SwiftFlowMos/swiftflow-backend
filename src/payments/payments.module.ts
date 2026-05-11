9fa6c8e7f094316d589f8bc24e9b066199f3d584
SwiftFlowMos <mostafa.belmoudden@gmail.com>
2026-05-11 22:02:20 +0000

    Update payments.module.ts

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 import { WorkflowService } from '../workflow/workflow.service';
+import { SystemAdaptersService } from '../system-adapters/system-adapters.service';
 import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [PaymentsController],
-  providers: [PaymentsService, WorkflowService, PrismaService],
+  providers: [PaymentsService, WorkflowService, SystemAdaptersService, PrismaService],
 })
 export class PaymentsModule {}
